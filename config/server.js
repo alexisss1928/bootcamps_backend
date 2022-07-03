40ded7ac4b16399f9f42b1a2854d46917e0e65ba
Alexis Salcedo <ale6jss@gmail.com>
2022-07-03 12:29:15 -0400

    Updated Yarn lockfile

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['qDDnsHLiU9Ed2A9Md5b8AA==','k9DVjtqmrfJqwaS3qKwFPw==','5LYlg+fgvOrBpXcbPNPgMA==','KxeeapCUfiPCQc9EajZX3Q==']),
   },
 });
